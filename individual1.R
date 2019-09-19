174d869f7eba0a5e56c802e52755fa037d64cb4c
Matthew <matthewp@iastate.edu>
2019-09-19 08:43:25 -0500

    the indevidual part of lab2

---
+++ individual1.R
@@ -0,0 +1,45 @@
+---
+  title: 'DS 202 - Lab 2: Exploring Chocolates'
+subtitle: "Due: Thursday 09/19/2019 by 10 pm"
+author: "Matthew Peterson"
+date: "09/12/2019"
+output: html_document
+---
+  
+  
+  ## Outline
+  
+  - Pair up and find your team member
+- Create a repository (one per team) & give access to your teammate
+- Do a data exploration (on chocolate ratings)
+- Create R markdown files that contain code to answer the questions and create the plots
+- Submit!
+  
+  
+  
+  ## Background
+  
+  Brady Brelinski, Founding Member of the Manhattan Chocolate Society, obviously likes chocolate. He has assembled a database of ratings for plain dark chocolate bars: [Flavors of chocolate](http://flavorsofcacao.com/chocolate_database.html)
+
+By now, over 1900 different chocolate bars have been rated. 
+
+## Meaning of ratings
+
+Flavors of Cacao Rating System:
+  
+- 5= Elite (Transcending beyond the ordinary limits)
+- 4= Premium (Superior flavor development, character and style)
+- 3= Satisfactory (3.0) to praiseworthy (3.75) (well made with special qualities)
+- 2= Disappointing (Passable but contains at least one significant flaw)
+- 1= Unpleasant (mostly unpalatable)
+
+
+## Question set (individual)
+
+Coordinate with your teammate and decide among you who is Individual 1 and Individual 2, respectively. Each of you should create an RMarkdown file either named `individual1.Rmd` or `individual2.Rmd` to include the answers.
+
+**For each of the questions below provide at least one graphic as a basis for your answer.**
+  
+  1. How are ratings distributed? Don't forget to mention outliers, if there are any.
+  2. Do ratings depend on the cacao percentage of a chocolate bar? 
+  3. What class is the variable representing company locations? How is this different from a 'character' variable? (You do not need to produce a graphic for this question.)
